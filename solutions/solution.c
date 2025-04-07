07fc0d0c9ec912e11feb0334dd4a065def7acfbf
sai krishna <saikrishnamamidi2003@gmail.com>
2025-04-07 13:03:50 +0530

    final commit

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,11 +1,5 @@
 int missingNumber(int* nums, int n) 
 {
-    int sum = 0,total = (n*(n+1))/2;
-    for(int i = 0;i<n;i++)
-    {
-        sum += nums[i];
-    }
-    return total-sum;
-      
+    //implement your code
 } 
  
